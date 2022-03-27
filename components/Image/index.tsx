265950d9ba58b1aef2488ff35f531fee6e4ea492
Spencer Carver <spencer.carver@gmail.com>
2022-03-26 23:13:14 -0400

    launch patch for image domains

--- components/Image/index.tsx
+++ components/Image/index.tsx
@@ -1,6 +1,16 @@
 import NextImage, { ImageProps } from "next/image";
 import { FunctionComponent } from "react";
 
-const Image: FunctionComponent<ImageProps> = (props) => <NextImage {...props} />;
+const NON_OPTIMIZED_DOMAINS = [
+    "c1.scryfall.com"
+];
+
+const Image: FunctionComponent<ImageProps> = (props) => {
+    if (typeof props.src === "string" && NON_OPTIMIZED_DOMAINS.find((domain) => (props.src as string).includes(domain))) {
+        return <NextImage { ...props } loader={ ({ src }) => src } />;
+    }
+
+    return <NextImage {...props} />;
+}
 
 export default Image;
