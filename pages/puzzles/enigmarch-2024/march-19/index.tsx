0e2e9d71cf8aa3b1f701b91bbb4114bec03f1db9
Spencer Carver <spencer.carver@gmail.com>
2024-03-19 21:26:38 -0400

    march-19 puzzle (#104)

--- pages/puzzles/enigmarch-2024/march-19/index.tsx
+++ pages/puzzles/enigmarch-2024/march-19/index.tsx
@@ -2,14 +2,61 @@ import React, { FunctionComponent } from "react";
 import { PuzzleWrapperComponent } from "../../../../components/Puzzle/common";
 import { styled } from "../../../../styles/stitches";
 
-const P = styled("p", {
-    color: "$onBackground"
+const UL = styled("ul", {
+    margin: "20px 0px",
+    marginLeft: "-20px",
+    "@lg": {
+        margin: "60px 20px"
+    }
 });
+
+const LI = styled("li", {
+    color: "$onBackground",
+    textAlign: "left",
+    fontSize: "20px",
+    paddingBottom: "10px",
+    listStyle: "none"
+});
+
+const ClueSpan = styled("span", {
+    display: "inline-block",
+    wordWrap: "normal",
+    maxWidth: "120px",
+    fontSize: "16px",
+    "@lg": {
+        maxWidth: "500px"
+    }
+});
+
+const P = styled("p", {
+    color: "$onBackground",
+    fontSize: "20px",
+});
+
+const CLUES = [
+    ["Fact", "True", "False"],
+    ["Holds the record for most disliked YouTube video for a video game trailer", "J", "S"],
+    ["Is the last World War II era Call of Duty game", "O", "L"],
+    ["Will be the 2024 release for the franchise", "C", "A"],
+    ["Contains the controversial level 'No Russian'", "P", "W"],
+    ["Has more of the 'Standalone' category releases than the even numbered titles", "Y", "R"],
+];
 
 const PuzzleComponent: FunctionComponent = () => {
     return (
         <PuzzleWrapperComponent name="enigmarch-2024:march-19">
-            <P>Coming Soon!</P>
+            <UL>
+                { CLUES.map((clue, index) => (
+                    <LI key={ index } css={ index === 0 ? { fontWeight: "bold", textDecoration: "underline" } : {} }>
+                        <ClueSpan>{ clue[0] }</ClueSpan>
+                        <span style={{ float: "right" }}>
+                            <span style={{ display: "inline-block", width: "50px", textAlign: "center" }}>{ clue[1] }</span>
+                            <span style={{ display: "inline-block", width: "50px", textAlign: "center" }}>{ clue[2] }</span>
+                        </span>
+                    </LI>
+                )) }
+            </UL>
+            <P>Then use your result with the number of odd entries developed by Treyarch and a method attributed to a character from a cancelled Vicarious Visions entry to the franchise!</P>
         </PuzzleWrapperComponent>
     );
 };
