f0dd42b360ac866693f1bf7165fff247002c88c1
Spencer Carver <spencer.carver@gmail.com>
2024-03-21 21:26:56 -0400

    march-21 puzzle

--- pages/puzzles/enigmarch-2024/march-21/index.tsx
+++ pages/puzzles/enigmarch-2024/march-21/index.tsx
@@ -1,15 +1,53 @@
 import React, { FunctionComponent } from "react";
+import Image from "../../../../components/Image";
 import { PuzzleWrapperComponent } from "../../../../components/Puzzle/common";
 import { styled } from "../../../../styles/stitches";
 
-const P = styled("p", {
-    color: "$onBackground"
+const SRC = "/puzzles/enigmarch-2024/overcooked.png";
+
+const WarningDiv = styled("div", {
+    margin: "10px 0",
+    color: "$onError",
+    backgroundColor: "$error",
+    "@lg": {
+        display: "none"
+    }
+});
+
+const ImageWrapperDiv = styled("div", {
+    position: "relative",
+    width: "300px",
+    height: "205px",
+    margin: "0 auto",
+    "@md": {
+        width: "450px",
+        height: "307px",
+        marginLeft: "-75px"
+    },
+    "@lg": {
+        width: "700px",
+        height: "477px",
+        marginLeft: "auto"
+    },
+    "@xl": {
+        width: "1000px",
+        height: "682px",
+        marginLeft: "-120px"
+    },
+    "@xxl": {
+        width: "1100px",
+        height: "750px",
+        marginLeft: "-170px"
+    }
 });
 
 const PuzzleComponent: FunctionComponent = () => {
     return (
         <PuzzleWrapperComponent name="enigmarch-2024:march-21">
-            <P>Coming Soon!</P>
+            <WarningDiv>{ "\u00a1\u00a1\u00a1WARNING: This puzzle requires a device large enough to read the image clearly!!!" }</WarningDiv>
+            <ImageWrapperDiv>
+                <Image src={ SRC } alt="A graph of ingredients in 'Overcooked 2'" layout="fill" />
+            </ImageWrapperDiv>
         </PuzzleWrapperComponent>
     );
 };
