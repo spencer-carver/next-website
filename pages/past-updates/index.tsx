d4d39f8aaa711ce2c2634b31bb5e979731d8f454
Spencer Carver <spencer.carver@gmail.com>
2025-03-01 19:40:09 -0500

    enigmarch-2025 start (#123)

--- pages/past-updates/index.tsx
+++ pages/past-updates/index.tsx
@@ -60,6 +60,100 @@ const PastUpdates: FunctionComponent<PageProps> = ({ theme }) => {
                 <BackNavigation to="/" />
                 <ConstructionGif theme={ theme } />
                 <Heading>Past Site News:</Heading>
+                <SubHeading>December 31, 2023</SubHeading>
+                <List>
+                    <ListItem>New Magic Deck: <Link href="/magic/deck/pramikon" component={ A }>Pramikon, Sky Rampart</Link></ListItem>
+                    <ListItem>New Magic Deck: <Link href="/magic/deck/gavi" component={ A }>Gavi, Nest Warden</Link></ListItem>
+                    <ListItem>Updated Magic Deck: <Link href="/magic/deck/hanna" component={ A }>Hanna, Ship&apos;s Navigator</Link></ListItem>
+                </List>
+                <P>Happy New Year!</P>
+                <P>Minor changes to some magic related content for now. The &apos;guidance&apos; section of some existing magic sections are also going to be re-written in the near future. This should better align with what they are meant to be (my thoughts on the deck, interesting synergies) rather than explicit matchup instructions, or other knowledge which can go stale (and which currently very much is).</P>
+                <SubHeading>December 2, 2023</SubHeading>
+                <List>
+                    <ListItem>New Puzzle: <Link href="/puzzles/speed-climbing" component={ A }>Speed Climbing</Link></ListItem>
+                    <ListItem>New Puzzle Solution: <Link href="/puzzles/an-explosive-discovery" component={ A }>An Explosive Discovery</Link></ListItem>
+                    <ListItem>New Puzzle Solution: <Link href="/puzzles/yakuza-zero" component={ A }>Yakuza Zero</Link></ListItem>
+                    <ListItem>New Puzzle Solution: <Link href="/puzzles/enigmarch-2022" component={ A }>#Enigmarch 2022</Link></ListItem>
+                </List>
+                <P>A LONG break! More puzzle answers are still coming Soon™ (I promise), but for now the first speed puzzle for my site, and themed around speed climbing to boot!</P>
+                <SubHeading>June 14, 2023</SubHeading>
+                <P>A long break! Puzzle answers are still coming Soon™, but for now updated some magic decks in the meantime.</P>
+                <SubHeading>April 24, 2023</SubHeading>
+                <P>Recipes have moved to <Link href="https://dumpling.academy" component={ A }>Dumpling Academy</Link>, my second website.</P>
+                <SubHeading>April 23, 2023</SubHeading>
+                <List>
+                    <ListItem>Updated Page: <Link href="https://dumpling.academy/recipes" component={ A }>Recipes</Link></ListItem>
+                </List>
+                <P>Still working on more features and better functionality here, but the main recipes page can now be populated with the contents of our shared recipe folder, and has rudimentary filtering based on tags.</P>
+                <SubHeading>April 18, 2023</SubHeading>
+                <List>
+                    <ListItem>Incoming support: Recipes</ListItem>
+                </List>
+                <P>Extending the blog structure to allow for Kathy&apos;s recipes to start filling that section of the site. More details TBD, but for now, <Link component={ A } href="https://dumpling.academy/recipes/dutch-baby">direct links</Link> work, but they aren&apos;t yet searchable.</P>
+                <SubHeading>April 9, 2023</SubHeading>
+                <List>
+                    <ListItem>New Page: <Link href="/past-updates" component={ A }>Past Updates</Link></ListItem>
+                    <ListItem>New Page: <Link href="/legal" component={ A }>Privacy Policy</Link></ListItem>
+                    <ListItem>Minor bug fixes</ListItem>
+                </List>
+                <P>Nothing glamourous today. Moving site updates older than a few months to their own page, officially adding a privacy policy considering the stats feature relies on a user Id, and fixing some bugs.</P>
+                <SubHeading>April 8, 2023</SubHeading>
+                <List>
+                    <ListItem>New feature: Puzzle Stats!</ListItem>
+                </List>
+                <P>Solution guides are still on their way, but was more motivated to work on something else right away. The answer checker will now keep statistics about number of attempts and incorrect guesses globally! For puzzles that have a solution guide available, these stats are now showcased at the top of that page. Check out the <Link href="/puzzles/tutorial" component={ A }>Tutorial</Link> puzzle for an example.</P>
+                <SubHeading>March 2023</SubHeading>
+                <List>
+                    <ListItem>Updated Puzzle Round: <Link href="/puzzles/enigmarch-2023" component={ A }>#Enigmarch 2023</Link></ListItem>
+                    <ListItem>New Puzzle: <Link href="/puzzles/enigmarch-2023/meta" component={ A }>March: META</Link></ListItem>
+                    <ListItem>New Puzzle: <Link href="/puzzles/enigmarch-2023/march-31" component={ A }>March 31: REVERSE</Link></ListItem>
+                    <ListItem>New Puzzle: <Link href="/puzzles/enigmarch-2023/march-30" component={ A }>March 30: PUSH</Link></ListItem>
+                    <ListItem>New Puzzle: <Link href="/puzzles/enigmarch-2023/march-29" component={ A }>March 29: QUOTE</Link></ListItem>
+                    <ListItem>New Puzzle: <Link href="/puzzles/enigmarch-2023/march-28" component={ A }>March 28: DIAGONAL</Link></ListItem>
+                    <ListItem>New Puzzle: <Link href="/puzzles/enigmarch-2023/march-27" component={ A }>March 27: FIRE</Link></ListItem>
+                    <ListItem>New Puzzle: <Link href="/puzzles/enigmarch-2023/march-26" component={ A }>March 26: GROW</Link></ListItem>
+                    <ListItem>New Puzzle: <Link href="/puzzles/enigmarch-2023/march-25" component={ A }>March 25: KNOT</Link></ListItem>
+                    <ListItem>New Puzzle: <Link href="/puzzles/enigmarch-2023/march-24" component={ A }>March 24: CRYPT</Link></ListItem>
+                    <ListItem>New Puzzle: <Link href="/puzzles/enigmarch-2023/march-23" component={ A }>March 23: DICE</Link></ListItem>
+                    <ListItem>New Puzzle: <Link href="/puzzles/enigmarch-2023/march-22" component={ A }>March 22: VISION</Link></ListItem>
+                    <ListItem>New Puzzle: <Link href="/puzzles/enigmarch-2023/march-21" component={ A }>March 21: HALVE</Link></ListItem>
+                    <ListItem>New Puzzle: <Link href="/puzzles/enigmarch-2023/march-20" component={ A }>March 20: FISH</Link></ListItem>
+                    <ListItem>New Puzzle: <Link href="/puzzles/enigmarch-2023/march-19" component={ A }>March 19: SPACE</Link></ListItem>
+                    <ListItem>New Puzzle: <Link href="/puzzles/enigmarch-2023/march-18" component={ A }>March 18: ROYAL</Link></ListItem>
+                    <ListItem>New Puzzle: <Link href="/puzzles/enigmarch-2023/march-17" component={ A }>March 17: SHELL</Link></ListItem>
+                    <ListItem>New Puzzle: <Link href="/puzzles/enigmarch-2023/march-16" component={ A }>March 16: MASK</Link></ListItem>
+                    <ListItem>New Puzzle: <Link href="/puzzles/enigmarch-2023/march-15" component={ A }>March 15: ROOM</Link></ListItem>
+                    <ListItem>New Puzzle: <Link href="/puzzles/enigmarch-2023/march-14" component={ A }>March 14: SECOND</Link></ListItem>
+                    <ListItem>New Puzzle: <Link href="/puzzles/enigmarch-2023/march-13" component={ A }>March 13: MATCH</Link></ListItem>
+                    <ListItem>New Puzzle: <Link href="/puzzles/enigmarch-2023/march-12" component={ A }>March 12: TOWER</Link></ListItem>
+                    <ListItem>New Puzzle: <Link href="/puzzles/enigmarch-2023/march-11" component={ A }>March 11: BOLD</Link></ListItem>
+                    <ListItem>New Puzzle: <Link href="/puzzles/enigmarch-2023/march-10" component={ A }>March 10: HAZARD</Link></ListItem>
+                    <ListItem>New Puzzle: <Link href="/puzzles/enigmarch-2023/march-9" component={ A }>March 9: GLITCH</Link></ListItem>
+                    <ListItem>New Puzzle: <Link href="/puzzles/enigmarch-2023/march-8" component={ A }>March 8: CURSE</Link></ListItem>
+                    <ListItem>New Puzzle: <Link href="/puzzles/enigmarch-2023/march-7" component={ A }>March 7: ARROW</Link></ListItem>
+                    <ListItem>New Puzzle: <Link href="/puzzles/enigmarch-2023/march-6" component={ A }>March 6: MYTHICAL</Link></ListItem>
+                    <ListItem>New Puzzle: <Link href="/puzzles/enigmarch-2023/march-5" component={ A }>March 5: GEM</Link></ListItem>
+                    <ListItem>New Puzzle: <Link href="/puzzles/enigmarch-2023/march-4" component={ A }>March 4: LIGHT</Link></ListItem>
+                    <ListItem>New Puzzle: <Link href="/puzzles/enigmarch-2023/march-3" component={ A }>March 3: WIND</Link></ListItem>
+                    <ListItem>New Puzzle: <Link href="/puzzles/enigmarch-2023/march-2" component={ A }>March 2: NEST</Link></ListItem>
+                    <ListItem>New Puzzle: <Link href="/puzzles/enigmarch-2023/march-1" component={ A }>March 1: KEY</Link></ListItem>
+                </List>
+                <P>Happy #Enigmarch! Each day I&apos;ll be adding a new puzzle from the inspirational prompts posted on the <Link href="https://enigmarch.com/prompts/2023-archive/" component={ A }>Enigmarch website</Link>.</P>
+                <SubHeading>February 25, 2023</SubHeading>
+                <List>
+                    <ListItem>Updated Page: <Link href="/about" component={ A }>About Me</Link></ListItem>
+                    <ListItem>New Puzzle: <Link href="/puzzles/130-bpm" component={ A }>130 BPM</Link></ListItem>
+                </List>
+                <P>Section for conference talks / publications added.</P>
+                <P>A new puzzle has been added! Right before #Enigmarch as well (where I plan to add 31 puzzles over 31 days). This puzzle is the implementation of an idea I have had for a long time, with heavy inspiration from a particularly wonderful MIT Mysteryhunt puzzle from 2020.</P>
+                <SubHeading>January 28, 2023</SubHeading>
+                <List>
+                    <ListItem>Updated Page: <Link href="/puzzles" component={ A }>Puzzle landing page</Link></ListItem>
+                    <ListItem>New Puzzle Round: <Link href="/puzzles/enigmarch-2023" component={ A }>#Enigmarch 2023</Link></ListItem>
+                    <ListItem>Various Site Enhancements</ListItem>
+                </List>
+                <P>While we are still over a month away from March, I set up some of the scaffolding for the puzzles to come!</P>
+                <P>Additionally, made some caching, storage, and timeout related updates to the site to (hopefully) allow things to load quicker, more reliably, and using less browser resources.</P>
                 <SubHeading>January 27, 2023</SubHeading>
                 <P>Myr myr myr! A new legendary creature has prompted a long-time stalwart of the decks page to get an update! Introducing <Link href="/magic/deck/urtet" component={ A }>Urtet, Remnant of Memnarch</Link>!</P>
                 <SubHeading>January 13, 2023</SubHeading>
@@ -156,7 +250,7 @@ const PastUpdates: FunctionComponent<PageProps> = ({ theme }) => {
                 <List>
                     <ListItem>New Puzzle: <Link href="/puzzles/enigmarch-2022" component={ A }>#Enigmarch 2022</Link></ListItem>
                 </List>
-                <P>Yesterday I found out from a friend about <Link href="https://enigmarch.com/" component={ A }>#Enigmarch</Link>, a month long puzzle creation challenge. Well it&apos;s VERY far into March at the moment, but I wanted to participate, and as such have created a single puzzle with the <Tooltip tooltip="Day 30's theme is PHANTOM, so this is close right?">spirit</Tooltip> of a month-long challenge (though I couldn&apos;t find a way to work the daily prompts into each of my days unfortunately). Looking forward to tackling it day-by-day next year!</P>
+                <P>Yesterday I found out from a friend about <Link href="https://enigmarch.com/" component={ A }>#Enigmarch</Link>, a month long puzzle creation challenge. Well it&apos;s VERY far into March at the moment, but I wanted to participate, and as such have created a single puzzle with the <Tooltip tooltip="Day 30's theme is PHANTOM, so this is close right?">spirit</Tooltip> of a month-long challenge (though I couldn&apos;t find a way to work the <Link href="https://enigmarch.com/prompts/2022-archive/" component={ A }>daily prompts</Link> into each of my days unfortunately). Looking forward to tackling it day-by-day next year!</P>
                 <SubHeading>March 28, 2022</SubHeading>
                 <P>Nothing notable here aside from a bunch of minor styling updates noticed now that I have a blog post to look at. I also updated the page metadata for unfurling (e.g. when I post pages to Facebook or Twitter, it will now have details about that page rather than just the site) and messed around with a few of the &apos;noscript&apos; settings, but would be wholely surprised if anyone legitimately encountered those.</P>
                 <SubHeading>March 27, 2022</SubHeading>
---
+++ pages/puzzles/enigmarch-2025/index.tsx
@@ -0,0 +1,146 @@
+import React, { FunctionComponent, useCallback, useEffect, useState } from "react";
+import { PuzzleWrapperComponent } from "../../../components/Puzzle/common";
+import useStorage from "../../../utils/useStorage";
+import { styled } from "../../../styles/stitches";
+import { PartialAnswerCheck } from "../../../components/Puzzle/AnswerCheck";
+import { Calendar, DayOfMonth, TableCell, WrapperDiv, buttonCellStyles } from "../enigmarch-2024";
+import Link from "../../../components/Link";
+
+const NAME = "enigmarch-2025";
+
+const LATEST_PUZZLE_MADE = 1;
+
+const MARCH_2025 = [
+    [0,0,0,0,0,0,1],
+    [2,3,4,5,6,7,8],
+    [9,10,11,12,13,14,15],
+    [16,17,18,19,20,21,22],
+    [23,24,25,26,27,28,29],
+    [30,31,0,0,0,0,0]
+];
+
+const MARCH_2025_VALUES = [
+    ["","","","","","","🌀"],
+    ["","","","","","",""],
+    ["","","","","","",""],
+    ["","","","","","",""],
+    ["","","","","","",""],
+    ["","","","","","",""]
+];
+
+const PUZZLE_LINKS = [
+    ["FLOW", "https://spencer-carver.github.io/diagram/enigmarch-2025?day=1"]
+];
+
+const P = styled("p", {
+    color: "$onBackground"
+});
+
+const FinalAnswerComponent = ({ intermediates, activeDay, onClickDate, }) => {
+    return (
+        <WrapperDiv>
+            <b>March 2025</b>
+            <Calendar>
+                <thead>
+                    <tr>
+                        <TableCell>Sun</TableCell>
+                        <TableCell>Mon</TableCell>
+                        <TableCell>Tue</TableCell>
+                        <TableCell>Wed</TableCell>
+                        <TableCell>Thu</TableCell>
+                        <TableCell>Fri</TableCell>
+                        <TableCell>Sat</TableCell>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        MARCH_2025.map((row, rowIndex) => {
+                            return (
+                                <tr key={ rowIndex }>
+                                    {
+                                        row.map((cell, columnIndex) => {
+                                            if (!cell) {
+                                                return <TableCell key={ `cell-${ rowIndex }-${ columnIndex }` } />
+                                            }
+
+                                            if (cell > LATEST_PUZZLE_MADE) {
+                                                return (
+                                                    <TableCell key={ `cell-${ rowIndex }-${ columnIndex }` }>
+                                                        <DayOfMonth>{cell}</DayOfMonth>
+                                                    </TableCell>
+                                                );
+                                            }
+
+                                            const additionalStyles = {
+                                                ...buttonCellStyles,
+                                                ...(intermediates[cell] ? { backgroundColor: "$secondary", color: "$onSecondary" } : {}),
+                                                ...(activeDay === cell ? { backgroundColor: "$primary", color: "$onPrimary", "&:hover": undefined } : {}),
+                                            };
+
+                                            const intermediateValue = intermediates[cell]?.split("|").length > 1 ? intermediates[cell].split("|")[1] : intermediates[cell];
+
+                                            return (
+                                                <TableCell key={ `cell-${ rowIndex }-${ columnIndex }` } role="button" onClick={ () => onClickDate(cell) } title={ `March ${ cell }` } css={ additionalStyles }>
+                                                    <DayOfMonth>{cell}</DayOfMonth>{ intermediateValue ? MARCH_2025_VALUES[rowIndex][columnIndex] : "" }
+                                                </TableCell>
+                                            );
+                                        })
+                                    }
+                                </tr>
+                            );
+                        })
+                    }
+                </tbody>
+            </Calendar>
+        </WrapperDiv>
+    );
+};
+
+const PuzzleComponent: FunctionComponent = () => {
+    const storage = useStorage("puzzle");
+    const [ intermediates, setIntermediates ] = useState(new Array(32));
+    const [ activeDay, setActiveDay ] = useState(0);
+
+    useEffect(() => {
+        try {
+            const storedIntermediates = storage.getItem<string[]>("enigmarch-2025-intermediates");
+
+            if (!storedIntermediates) {
+                return;
+            }
+
+            setIntermediates(storedIntermediates);
+        } catch (e) {
+            //do nothing
+        }
+    }, [storage]);
+
+    const completeStep = useCallback((step: number, value: string) => {
+        const newIntermediates = intermediates.slice();
+        newIntermediates[step] = value;
+        setIntermediates(newIntermediates);
+
+        try {
+            storage.setItem<string[]>("enigmarch-2025-intermediates", newIntermediates);
+        } catch (e) {
+            //do nothing
+        }
+    }, [storage, intermediates, setIntermediates]);
+
+    return (
+        <PuzzleWrapperComponent name={ NAME }>
+            <div style={{ position: "relative", maxWidth: "740px", paddingLeft: "10px" }}>
+                <FinalAnswerComponent intermediates={ intermediates } activeDay={ activeDay } onClickDate={ (date) => setActiveDay(date) } />
+            </div>
+            <P>Each day a puzzle will be created at some external location! If you solve it, make sure to come back and enter the final answer for the meta puzzle!</P>
+            <div style={{ marginTop: "50px" }}>
+                { activeDay !== 0 && <Link href={ PUZZLE_LINKS[activeDay - 1][1] }>March { activeDay }: { PUZZLE_LINKS[activeDay - 1][0] }</Link> }
+            </div>
+            <div style={{ marginTop: "50px" }}>
+                { activeDay !== 0 && <PartialAnswerCheck puzzleName={ NAME } step={ activeDay } completeStep={ completeStep } placeholderText="Today's Answer Here" partialAnswer={ intermediates[activeDay]?.split("|")[0] } /> }
+            </div>
+        </PuzzleWrapperComponent>
+    );
+};
+
+export default PuzzleComponent;
