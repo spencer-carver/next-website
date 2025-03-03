4f3dec03108d1f2311a98cd151db30e5a85d78d8
Spencer Carver <spencer.carver@gmail.com>
2025-03-02 22:43:07 -0500

    enigmarch 2025 day 3

--- pages/puzzles/enigmarch-2025/index.tsx
+++ pages/puzzles/enigmarch-2025/index.tsx
@@ -8,7 +8,7 @@ import Link from "../../../components/Link";
 
 const NAME = "enigmarch-2025";
 
-const LATEST_PUZZLE_MADE = 2;
+const LATEST_PUZZLE_MADE = 3;
 
 const MARCH_2025 = [
     [0,0,0,0,0,0,1],
@@ -21,7 +21,7 @@ const MARCH_2025 = [
 
 const MARCH_2025_VALUES = [
     ["","","","","","","🌀"],
-    ["🐴","","","","","",""],
+    ["🐴","🦚","","","","",""],
     ["","","","","","",""],
     ["","","","","","",""],
     ["","","","","","",""],
@@ -30,7 +30,8 @@ const MARCH_2025_VALUES = [
 
 const PUZZLE_LINKS = [
     ["FLOW", "https://spencer-carver.github.io/diagram/enigmarch-2025?day=1"],
-    ["STAFF", "https://spencer-carver.github.io/diagram/enigmarch-2025?day=2"]
+    ["STAFF", "https://spencer-carver.github.io/diagram/enigmarch-2025?day=2"],
+    ["PSYCHIC", "https://spencer-carver.github.io/diagram/enigmarch-2025?day=3"]
 ];
 
 const P = styled("p", {
