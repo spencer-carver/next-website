09eb2cda65fa0dcf8176f3c1237c20d2c8fefde8
Spencer Carver <spencer.carver@gmail.com>
2025-03-20 21:13:43 -0400

    enigmarch 2025 day 19 and 20

--- pages/puzzles/enigmarch-2025/index.tsx
+++ pages/puzzles/enigmarch-2025/index.tsx
@@ -8,7 +8,7 @@ import Link from "../../../components/Link";
 
 const NAME = "enigmarch-2025";
 
-const LATEST_PUZZLE_MADE = 19;
+const LATEST_PUZZLE_MADE = 21;
 
 const MARCH_2025 = [
     [0,0,0,0,0,0,1],
@@ -23,7 +23,7 @@ const MARCH_2025_VALUES = [
     ["","","","","","","🌀"],
     ["🐴","🦚","🐂","🐆","🦉","🐱","🎖️"],
     ["🏖️","🦡","👷","🐻","🤠","🃏","🐮"],
-    ["🤵","☘️","🐃","🚂","","",""],
+    ["🤵","☘️","🐃","🚂","🤐","🐇",""],
     ["","","","","","",""],
     ["","","","","","",""]
 ];
@@ -47,7 +47,9 @@ const PUZZLE_LINKS = [
     ["BOW", "https://spencer-carver.github.io/diagram/enigmarch-2025?day=16"],
     ["LUCKY", "https://spencer-carver.github.io/diagram/enigmarch-2025?day=17"],
     ["PAUSE", "https://spencer-carver.github.io/diagram/enigmarch-2025?day=18"],
-    ["MACHINE", "https://spencer-carver.github.io/diagram/enigmarch-2025?day=19"]
+    ["MACHINE", "https://spencer-carver.github.io/diagram/enigmarch-2025?day=19"],
+    ["CASE", "https://spencer-carver.github.io/diagram/enigmarch-2025?day=20"],
+    ["COMBINE", "https://spencer-carver.github.io/diagram/enigmarch-2025?day=21"]
 ];
 
 const P = styled("p", {
